31e051a4365f9d27e90d0612acf5b5644c6a19ce
jane <sysoievayevheniia@gmail.com>
2025-06-20 20:21:32 +0300

    added hw13_2, edited extra task

--- extra_tasks/task6.py
+++ extra_tasks/task6.py
@@ -1,38 +1,38 @@
-# """
-# Створи клас Book, який має:
-# 🔸 Поля:
-# title — назва книги
-# author — автор
-# pages — кількість сторінок
-#
-# 🔸 Методи:
-# description(self) — звичайний метод, повертає рядок типу:
-# "Назва: Гаррі Поттер, Автор: Дж.К. Ролінг, Сторінок: 500"
-#
-# @staticmethod def is_long_book(pages) — статичний метод, який повертає:
-# True, якщо сторінок більше ніж 300
-# False — інакше
-# """
-# class Book:
-#     def __init__(self, title, author, pages):
-#         self.title = title
-#         self.author = author
-#         self.pages = pages
-#
-#     def description(self):
-#         return f"Назва: {self.title}, Автор: {self.author}, {self.pages}"
-#
-#     @staticmethod
-#     def is_long_book(pages):
-#         if pages > 300:
-#             return True
-#         return False
-#
-#
-# book = Book("Гаррі Поттер", "Дж.К. Ролінг", 500)
-# print(book.description())                     # Назва: Гаррі Поттер, Автор: Дж.К. Ролінг, Сторінок: 500
-# print(Book.is_long_book(500))                 # True
-# print(Book.is_long_book(120))                 # False
-#
+"""
+Створи клас Book, який має:
+🔸 Поля:
+title — назва книги
+author — автор
+pages — кількість сторінок
+
+🔸 Методи:
+description(self) — звичайний метод, повертає рядок типу:
+"Назва: Гаррі Поттер, Автор: Дж.К. Ролінг, Сторінок: 500"
+
+@staticmethod def is_long_book(pages) — статичний метод, який повертає:
+True, якщо сторінок більше ніж 300
+False — інакше
+"""
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def description(self):
+        return f"Назва: {self.title}, Автор: {self.author}, {self.pages}"
+
+    @staticmethod
+    def is_long_book(pages):
+        if pages > 300:
+            return True
+        return False
+
+
+book = Book("Гаррі Поттер", "Дж.К. Ролінг", 500)
+print(book.description())                     # Назва: Гаррі Поттер, Автор: Дж.К. Ролінг, Сторінок: 500
+print(Book.is_long_book(500))                 # True
+print(Book.is_long_book(120))                 # False
+
 
 
